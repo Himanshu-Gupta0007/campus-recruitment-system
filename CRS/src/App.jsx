26b7536e6aa9ba59404fa95b28111d37d6129c1b
DB <guptahimu90@gmail.com>
2026-06-24 13:32:15 +0530

    first commit

--- CRS/src/App.jsx
+++ CRS/src/App.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 const App = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🎓 Campus Recruitment System</h1>
-      <p>Connecting Students, Recruiters, and Colleges.</p>
-      <button>Get Started</button>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600 mb-4">
+        🎓 Campus Recruitment System
+      </h1>
+
+      <p className="text-lg text-gray-600 mb-6">
+        Connecting Students, Recruiters, and Colleges.
+      </p>
+
+      <button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700">
+        Get Started
+      </button>
     </div>
   );
 };
