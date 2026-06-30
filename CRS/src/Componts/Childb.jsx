56e5f8d7d7c60dd10868a378efcf0205f53f8d04
DB <guptahimu90@gmail.com>
2026-06-30 12:44:06 +0530

    first commit

--- CRS/src/Componts/Childb.jsx
+++ CRS/src/Componts/Childb.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Childb = () => {
   return (
-    <div>Childb</div>
-  )
-}
+    <div
+      style={{
+        padding: "20px",
+        margin: "20px",
+        backgroundColor: "#f4f4f4",
+        borderRadius: "10px",
+        textAlign: "center",
+      }}
+    >
+      <h2>👦 Child B Component</h2>
+      <p>This is Child B.</p>
+    </div>
+  );
+};
 
-export default Childb
+export default Childb;
