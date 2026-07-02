aada968521854cfede537cd91e539169f35b13ab
DB <guptahimu90@gmail.com>
2026-07-02 12:09:47 +0530

    first commit

--- CRS/src/App.jsx
+++ CRS/src/App.jsx
@@ -1,43 +1,18 @@
 import React from 'react'
 import {useState , createContext} from 'react'
-import Chlidc from './Componts/Childc'
-//import Navbar from './Componts/Navbar'
+//import Chlidc from './Componts/Childc'
+import Navbar from './Componts/Navbar'
 //const Createcontext = Createcontext(ThemeProvider)
 
 const App = () => {
-  const ThemeContext   = createContext()
-  const [theme , setTheme] = useState('black')
+ 
 
   
   return (
     <div>
-      <ThemeContext.Provider value={theme}>
-        <Chlidc theme={theme} />
-      </ThemeContext.Provider>
+      
      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
+<Navbar />
     </div>
   )
 }
