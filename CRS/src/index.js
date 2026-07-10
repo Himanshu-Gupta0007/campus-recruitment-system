3d559cc7818a5637059b1750b6a6d2a918419f52
DB <guptahimu90@gmail.com>
2026-07-10 14:14:02 +0530

    Initial commit

--- CRS/src/index.js
+++ CRS/src/index.js
@@ -45,13 +45,14 @@ function maxProfit(prices) {
 
     let maxProfit = 0;
 
-    
+
     for (let i = 0; i < prices.length; i++) {
         if (prices[i] < minPrice) {
             minPrice = prices[i];
         } else if (prices[i] - minPrice > maxProfit) {
             maxProfit = prices[i] - minPrice;
         }
+        
     }
     return maxProfit;
 }   
