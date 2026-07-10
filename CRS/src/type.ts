612627f2f344243b84bd9ffe3886b9f4f2ea36f0
DB <guptahimu90@gmail.com>
2026-07-10 14:51:17 +0530

    Initial typescript

---
+++ CRS/src/type.ts
@@ -0,0 +1,19 @@
+
+
+
+
+let age: number = 22;
+let numbers: number[] = [1, 2, 3, 4];
+
+
+interface User {
+  name: string;
+  age: number;
+}
+
+const user: User = {
+  name: "Himanshu",
+  age: 22,
+};
+
+console.log(user);
