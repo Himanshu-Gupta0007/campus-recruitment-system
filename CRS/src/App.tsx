ac5042b73020877a8e6cebd900f01d472e3e8a1b
DB <guptahimu90@gmail.com>
2026-07-13 13:16:52 +0530

    typescript-basics

--- CRS/src/App.tsx
+++ CRS/src/App.tsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 const App = () => {
   const name: string = "Himanshu";
   const age: number = 22;
-  const [count , setCount] = useState<number>(0);
+  const [count , setCount] = useState<string>("0");
 
   return (
     <div>
       <h1>{name}</h1>
       <h2>{age}</h2>
-      <button  onClick={() => setCount(count + 1)}>+</button>
-      <h1>{count}</h1>
-      <button className="btn btn-primary" onClick={() => setCount(count - 1)}>-</button>
+      <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg transition duration-300 shadow-md hover:shadow-lg" onClick={() => setCount((parseInt(count) + 1).toString())}>+</button>
+      <h1 className="text-2xl font-bold">{count}</h1>
+      <button className="bg-red-600 hover:bg-red-700 text-white font-semibold px-6 py-3 rounded-lg transition duration-300 shadow-md hover:shadow-lg  " onClick={() => setCount((parseInt(count) - 1).toString())}>-</button>
     </div>
   );
 };
