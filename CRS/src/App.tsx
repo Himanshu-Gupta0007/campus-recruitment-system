0efc4bcb8eaa1d392190e72198770e15db0d130b
DB <guptahimu90@gmail.com>
2026-07-13 13:12:35 +0530

    typescript-basics

--- CRS/src/App.tsx
+++ CRS/src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 const App = () => {
   const name: string = "Himanshu";
   const age: number = 22;
+  const [count , setCount] = useState<number>(0);
 
   return (
     <div>
       <h1>{name}</h1>
       <h2>{age}</h2>
+      <button  onClick={() => setCount(count + 1)}>+</button>
+      <h1>{count}</h1>
+      <button className="btn btn-primary" onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 };
