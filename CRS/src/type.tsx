c065a3165e0a7ff6bedf230bac9a73168a924d84
DB <guptahimu90@gmail.com>
2026-07-13 12:25:46 +0530

    typescript-basics

--- CRS/src/type.tsx
+++ CRS/src/type.tsx
@@ -4,3 +4,20 @@
 let firstName: string = "Himanshu";
 
 console.log(firstName);
+
+
+let age: number = 22;
+
+console.log(age);
+
+let isStudent: boolean = true;
+
+console.log(isStudent);
+
+let city: string = "Delhi";
+let salary: number = 25000;
+let isMarried: boolean = false;
+
+console.log(city);
+console.log(salary);
+console.log(isMarried);
